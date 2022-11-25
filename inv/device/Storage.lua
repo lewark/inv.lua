d1aeac61fe87e9dc2f54a74dd17b47ff800cc432
knector01 <78086132+knector01@users.noreply.github.com>
2022-11-25 00:55:53 -0700

    Allow multiple inventory filters

--- inv/device/Storage.lua
+++ inv/device/Storage.lua
@@ -7,9 +7,12 @@ local Storage = Device:subclass()
 function Storage:init(server, name, deviceType, config)
     Storage.superClass.init(self, server, name, deviceType, config)
     self.priority = self.config.priority or 0
-    self.filter = nil
-    if self.config.filter then
-        self.filter = ItemCriteria(self.config.filter)
+    self.filters = nil
+    if self.config.filters then
+        self.filters = {}
+        for i, filter in ipairs(self.config.filters) do
+            table.insert(self.filters, ItemCriteria(filter))
+        end
     end
 
     self.server.invManager:addInventory(self)
@@ -20,7 +23,15 @@ function Storage:destroy()
 end
 
 function Storage:itemAllowed(item)
-    return not self.filter or self.filter:matches(item)
+    if self.filters then
+        for i,filter in ipairs(self.filters) do
+            if filter:matches(item) then
+                return true
+            end
+        end
+        return false
+    end
+    return true
 end
 
 function Storage:list()
