0a325b376e093e622fc13b50ed4906c8e2e1d664
knector01 <78086132+knector01@users.noreply.github.com>
2022-12-11 19:16:57 -0700

    Document TaskManager

--- inv/TaskManager.lua
+++ inv/TaskManager.lua
@@ -1,19 +1,28 @@
 local Object = require 'object.Object'
 
+-- Asynchronously manages crafting tasks
 local TaskManager = Object:subclass()
 
 function TaskManager:init(server)
     self.server = server
+    -- table<int, Task>: Tasks that are currently performing an operation,
+    -- e.g. counting and storing output items from a crafting machine.
     self.active = {}
+    -- table<int, Task>: Tasks that are waiting for another task to complete.
+    -- Indexed by task ID.
     self.sleeping = {}
+    -- The last ID assigned to a task.
     self.lastID = 0
 end
 
+-- Returns the next available task ID for creating a new task.
 function TaskManager:nextID()
     self.lastID = self.lastID + 1
     return self.lastID
 end
 
+-- Updates all running tasks, sleeping parent tasks when they create sub-tasks
+-- and resuming them when the sub-tasks complete.
 function TaskManager:update()
     --print("calling update")
     local i = 1
@@ -40,6 +49,7 @@ function TaskManager:update()
     return false
 end
 
+-- Adds a new task, and designates it as active.
 function TaskManager:addTask(task)
     table.insert(self.active, task)
 end
